790efa327913fb77a9297ce4250d3f78ca6f0301
Samy Bencherif <samybencherif@live.com>
2018-11-24 01:07:22 -0500

    added box2d physics

--- core/src/com/gdx/platformer2D/Platformer2D.java
+++ core/src/com/gdx/platformer2D/Platformer2D.java
@@ -5,13 +5,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 
 public class Platformer2D extends ApplicationAdapter {
 
@@ -20,9 +17,12 @@ public class Platformer2D extends ApplicationAdapter {
     float viewportWidth;
 
     World world;
+    Box2DDebugRenderer debugRenderer;
 
     @Override
 	public void create () {
+
+        /** Initialize Rendering */
 
         // this object is instantiated for custom rendering
 		shapeRenderer = new ShapeRenderer();
@@ -40,6 +40,61 @@ public class Platformer2D extends ApplicationAdapter {
         cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
         cam.update();
 
+        /** Initialize Physics */
+
+        world = new World(new Vector2(0, -10), true);
+
+        // temporary
+        debugRenderer = new Box2DDebugRenderer();
+
+        // First we create a body definition
+        BodyDef bodyDef = new BodyDef();
+        // We set our body to dynamic, for something like ground which doesn't move we would set it to StaticBody
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        // Set our body's starting position in the world
+        bodyDef.position.set(10, 20);
+
+        // Create our body in the world using our body definition
+        Body body = world.createBody(bodyDef);
+
+        // Create a circle shape and set its radius
+        CircleShape circle = new CircleShape();
+        circle.setRadius(2f);
+
+        // Create a fixture definition to apply our shape to
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = circle;
+        fixtureDef.density = 0.5f;
+        fixtureDef.friction = 0.4f;
+        fixtureDef.restitution = 0.6f; // Make it bounce a little bit
+
+        // Create our fixture and attach it to the body
+        Fixture fixture = body.createFixture(fixtureDef);
+
+        // Remember to dispose of any shapes after you're done with them!
+        // BodyDef and FixtureDef don't need disposing, but shapes do.
+        circle.dispose();
+
+
+        // Create our body definition
+        BodyDef groundBodyDef = new BodyDef();
+        // Set its world position
+        groundBodyDef.position.set(new Vector2(10, 1));
+
+        // Create a body from the definition and add it to the world
+        Body groundBody = world.createBody(groundBodyDef);
+
+        // Create a polygon shape
+        PolygonShape groundBox = new PolygonShape();
+        // Set the polygon shape as a box which is twice the size of our view port and 20 high
+        // (setAsBox takes half-width and half-height as arguments)
+        groundBox.setAsBox(8, 1.0f);
+
+        // Create a fixture from our polygon shape and add it to our ground body
+        groundBody.createFixture(groundBox, 0.0f);
+        // Clean up after ourselves
+        groundBox.dispose();
+
 	}
 
 	@Override
@@ -56,11 +111,16 @@ public class Platformer2D extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		// draw a red (2x2) square 2 meters from the left and bottom edges
-        shapeRenderer.begin(ShapeType.Filled);
-        shapeRenderer.setColor(.86f,.28f,.35f,1f);
-        shapeRenderer.rect(2,2,2,2);
-        shapeRenderer.end();
+//        shapeRenderer.begin(ShapeType.Filled);
+//        shapeRenderer.setColor(.86f,.28f,.35f,1f);
+//        shapeRenderer.rect(2,2,2,2);
+//        shapeRenderer.end();
 
+        // debug renderer
+        debugRenderer.render(world, cam.combined);
+
+        // simulate physics
+        world.step(1/60f, 6, 2);
 	}
 
     private void handleInput() {
@@ -76,8 +136,14 @@ public class Platformer2D extends ApplicationAdapter {
 
     @Override
     public void resize(int width, int height) {
+
         cam.viewportWidth = viewportWidth;
-        cam.viewportHeight = viewportWidth * (height/width);
+        float oldHeight = cam.viewportHeight;
+        cam.viewportHeight = viewportWidth * height/width;
+
+        // keep the bottom left corner fixed
+        cam.position.add(0,(cam.viewportHeight-oldHeight)/2,0);
+
         cam.update();
     }
 
